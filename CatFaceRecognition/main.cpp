eaea7af8c23ca76fd979bef558c1a83ff74a6cd0
Konrad Lisiecki <knrd.lisiecki@gmail.com>
2015-02-03 17:10:15 +0100

    CV: change colour of image to sepia or grayscale

--- CatFaceRecognition/main.cpp
+++ CatFaceRecognition/main.cpp
@@ -18,6 +18,8 @@
 #include<stdlib.h>
 #include<unistd.h>
 
+double alpha; /**< Simple contrast control */
+int beta;  /**< Simple brightness control */
 
 using namespace std;
 using namespace cv;
@@ -27,30 +29,100 @@ double max_face_size=2000;
 Mat mask;
 int SIZE = 1024;
 
+#pragma pack(push, 2)
+struct RGB {        //members are in "bgr" order!
+    uchar blue;
+    uchar green;
+    uchar red;
+};
+
 pthread_t tid[2];
 int counter;
 pthread_mutex_t mutexx = PTHREAD_MUTEX_INITIALIZER;
-std::string imageName = "./images/9.jpg";
+std::string imageName = "./images/pilot.jpg";
+const char* masks[] = {"batman", "bird", "carnival", "commandos", "horse", "monster", "oldman",
+                       "pig", "pilot", "tree", "venice", "spiderman"};
+std::vector<std::string> availableMasks(masks, masks + 12);
+bool stop = false;
+const char* modes[] = {"normal", "sepida", "gray"};
+std::vector<std::string> availableModes(modes, modes + 3);
+std::string mode = "normal";
 
-void* doSomeThing(void *arg)
-{
-    std::string name;
-    while (1) {
-        pthread_mutex_lock(&mutexx);
-        
-        std::cout << "Please, enter image name ";
-        std::getline (std::cin,name);
-        imageName = "./images/" + name + ".jpg";
-    
-        std::cout << imageName << std::endl;
-    
-        pthread_mutex_unlock(&mutexx);
-    }
-    return NULL;
-}
+
 
 Mat detectFace(Mat src);
 Mat putMask(Mat src,Point center,Size face_size);
+
+
+
+bool stopProgram(std::string input) {
+    return input == "stop";
+}
+
+bool validName(std::string input)
+{
+    return std::find(availableMasks.begin(), availableMasks.end(), input) != availableMasks.end();
+}
+
+void* doSomeThing(void *arg)
+{
+    using namespace std;
+    string input;
+    
+    while (1) {
+        cout << "Please, enter mask name (stop to exit program): \n";
+        cout << "Available masks: \n";
+        for (int i = 0; i < availableMasks.size(); i ++) {
+            cout << i+1 << ". "+ availableMasks.at(i) << endl;
+        }
+        getline (std::cin,input);
+        
+        if (validName(input)) {
+            pthread_mutex_lock(&mutexx);
+            cout << "You have changed to " << input << " mask\n";
+            std::string ext;
+            input == "horse" ? ext = "png" : ext = "jpg";
+            imageName = "./images/" + input + "." + ext;
+            pthread_mutex_unlock(&mutexx);
+        } else if (stopProgram(input)) {
+            stop = true;
+        } else {
+            cout << "Invalid name!\n";
+        }
+    }
+    return NULL;
+}
+Mat grayScale(Mat frame){
+    Mat gray = frame.clone();
+    for (int i= 0; i<frame.rows; ++i)
+    {
+        for (int j = 0 ; j < frame.cols; ++j)
+        {
+            int b = frame.ptr<RGB>(i)[j].blue;
+            int r = frame.ptr<RGB>(i)[j].red;
+            int g = frame.ptr<RGB>(i)[j].green;
+            int grayscale = ((r*77)+(b*28)+(g*151))/256;
+            gray.ptr<RGB>(i)[j].blue = grayscale;
+            gray.ptr<RGB>(i)[j].red = grayscale;
+            gray.ptr<RGB>(i)[j].green = grayscale;
+        }
+    }
+    // cv::Mat gray(frame.size(), CV_8UC1);
+    // cvtColor(frame, gray, CV_BGR2Luv);
+    return gray;
+}
+
+Mat sepia(Mat frame){
+    Mat sepia = frame.clone();
+    
+    Mat kern = (cv::Mat_<float>(4,4) <<  0.272, 0.534, 0.131, 0,
+                    0.349, 0.686, 0.168, 0,
+                    0.393, 0.769, 0.189, 0,
+                    0, 0, 0, 1);
+    
+    cv::transform(frame, sepia, kern);
+    return sepia;
+}
 
 
 int main( )
@@ -60,23 +132,29 @@ int main( )
     if (err != 0)
         printf("\ncan't create thread :[%s]", strerror(err));
     
-    
     VideoCapture cap(0);
     namedWindow( "window1", 1 );
     
-    while(1)
+    while(!stop)
     {
         mask = imread(imageName);
         Mat frame;
         cap >> frame;
-        frame=detectFace(frame);
         
-        imshow( "window1", frame );
-        // Press 'c' to escape
-        if(waitKey(1) == 'c') break;
+        cv::Mat imgDistance(frame.size(), CV_8UC1);
+        frame=detectFace(frame);
+
+        if (mode == "sepia") {
+            imshow( "window1", sepia(frame));
+        } else if (mode == "gray") {
+            imshow( "window1", grayScale(frame));
+        } else
+            imshow( "window1", frame);
+
+        waitKey(1);
     }
     
-    waitKey(0);
+    //waitKey(0);
     return 0;
 }
 
